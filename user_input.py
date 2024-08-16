86936438991bd65392d7c348b104182d9a19fbfa
WambiruL <wambirulorraine@gmail.com>
2024-08-16 12:11:15 +0300

    User Input assignment

---
+++ user_input.py
@@ -0,0 +1,18 @@
+name = input('Enter your name')
+age = input('Enter your age')
+location = input('Enter your location')
+
+print(f'Hello {name}, you are {age} years old and you live in {location}')
+
+# a = (1, 2, 3)
+# b = list(a)
+# print(b)
+
+# a = 10
+# b = 3
+# print(a % b)
+
+# x = {}
+# x[2] = 10
+# x[1] = [20, 30,40]
+# print(x[1][2])
